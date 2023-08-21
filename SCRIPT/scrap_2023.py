84dc0f34720e2a2f845cb9d3063347b859a024a0
Silvio Lima <silviolima07@gmail.com>
2023-08-21 20:34:31 -0300

    Update scrap_2023.py

--- SCRIPT/scrap_2023.py
+++ SCRIPT/scrap_2023.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/16uIF-PNDlEzFi3EUFSxgwcWpH-Vbo578
 """
 import pandas as pd
+import numpy as np
 import requests
 from bs4 import BeautifulSoup
 
